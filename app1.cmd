be78610a6dcb80dc48cefe5a353673bcfe931826
archnagupta1 <archna.gupta@mphasis.com>
2017-02-03 13:39:39 +0530

    Add files via upload

---
+++ app1.cmd
@@ -0,0 +1,4 @@
+cd D:\apache-groovy-binary-2.4.7\therore-zookeeperdump-master\therore-zookeeperdump-master
+set GROOVY_HOME=D:\apache-groovy-binary-2.4.7\groovy-2.4.7
+set PATH=%PATH%;D:\apache-groovy-binary-2.4.7\groovy-2.4.7\bin
+groovy zookeeperdump.groovy -c /config/director < application.yml
